3f6ad86ec78b0244a224c8aa757e94a21eaba393
CodeTech25 <senacmaterial24@gmail.com>
2025-08-11 18:29:45 -0300

    g

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ conectarBanco();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+//const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
