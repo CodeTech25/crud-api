99fac9ed16d80af2e94b905e136edb23bb52937b
CodeTech25 <senacmaterial24@gmail.com>
2025-08-11 18:37:33 -0300

    g

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,6 @@
 
 
-const API_URL = 'https://crud-api.onrender.com/api/contatos';
+const API_URL = 'https://crud-api.onrender.com/api';
 
 const form = document.getElementById('form-contato');
 const tabela = document.getElementById('tabela-contatos');
